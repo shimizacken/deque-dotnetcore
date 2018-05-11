53c2d9092c3c67e7db7b15665a4357105ef05ffb
dm\shimiz <Shimi.Zacken@checkmarx.com>
2018-05-11 14:20:59 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace deque_dotnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
